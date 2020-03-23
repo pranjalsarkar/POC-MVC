24e557212ac528b95a3ece50a053623ee1a3ecb6
Pranjal Sarkar <Pranjal.Sarkar@neudesic.com>
2020-03-23 21:14:39 +0530

    Add project files.

---
+++ Chat/Controllers/ChatController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chat.Models;
+using Newtonsoft.Json;
+
+namespace Chat.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<OrderList> dataModel;
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            dataModel = new List<OrderList>{
+                new OrderList() {CustomerName="ABC", OrderNo="161afec7-380a-43e6-9bf7-614d5210dbd9", TotalCost=150},
+                new OrderList() {CustomerName="XYZ", OrderNo="f8d4068e-e81f-4231-b623-6e64cf996b01", TotalCost=250}
+            };
+
+            return View(dataModel);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+         
+            if (dataModel == null)
+            {
+                dataModel = new List<OrderList>{
+                new OrderList() {CustomerName="ABC", OrderNo="161afec7-380a-43e6-9bf7-614d5210dbd9", TotalCost=150},
+                new OrderList() {CustomerName="XYZ", OrderNo="f8d4068e-e81f-4231-b623-6e64cf996b01", TotalCost=250}
+            };
+            }
+
+            return PartialView("Chat", dataModel);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Add()
+        {
+            var Model = new Order();
+            Model.OrderNo = Guid.NewGuid().ToString();
+            return PartialView(Model);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Mdl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public string CreateOrder(OrderList Mdl)
+        {
+            dataModel.Add(Mdl);
+            return JsonConvert.SerializeObject(dataModel);
+        }
+    }
+}
---
+++ Chat/Models/Chat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat.Models
+{
+    public class Order
+    {
+        public string OrderNo { get; set; }
+        public string CustomerName { get; set; }
+        public string item { get; set; }
+        public double Cost { get; set; }
+        public double CGST { get; set; }
+        public double SGST { get; set; }  
+    }
+
+    public class OrderList
+    {
+        public string OrderNo { get; set; }
+        public string CustomerName { get; set; }
+        public double TotalCost { get; set; }
+    } 
+}
